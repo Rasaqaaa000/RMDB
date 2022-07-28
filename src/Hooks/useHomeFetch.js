c42be071b941dd1b6f745a2ca0f7be328520160f
Abdulrasaq Aremu <aremuabdulrasaq00@gmail.com>
2022-07-28 14:22:11 +0100

    second commit

--- src/Hooks/useHomeFetch.js
+++ src/Hooks/useHomeFetch.js
@@ -9,11 +9,12 @@ const initialState = {
 }
 
 export const useHomeFetch = () => {
+    const [searchTerm, setSearchTerm] = React.useState('')
     const [state, setState] = React.useState()
     const [loading, setLoading] = React.useState(false)
     const [error, setError] = React.useState(false)
 
-    const fetchMovies = async (page, searchTerm = "") => {
+    const fetchMovies = async (page, searchTerm = '') => {
         try {
             setError(false)
             setLoading(true)
@@ -34,8 +35,9 @@ export const useHomeFetch = () => {
     }
     //initial render
     React.useEffect(() => {
-        fetchMovies(1)
-    }, [])
+        // setState(initialState)
+        fetchMovies(1, searchTerm)
+    }, [searchTerm])
 
-    return { state, loading, error }
+    return { state, loading, error, setSearchTerm }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { useHomeFetch } from '../Hooks/useHomeFetch'
 import HeroImage from './HeroImage'
+import Grid from './Grid'
+import Thumb from './Thumb'
+import Spinner from './Spinner'
+import SearchBar from './SearchBar'
 //API
 
 //config
@@ -10,17 +14,32 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 import NoImage from '../images/no_image.jpg'
 
 const Home = () => {
-    const { state, loading, error } = useHomeFetch
+    const { state, loading, error, setSearchTerm } = useHomeFetch()
     return (
         <>
-            {/* {state.results[0] ? (<HeroImage
+            {state?.results[0] ? (<HeroImage
                 image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
                 text={state.results[0].overview}
                 title={state.results[0].original_title}
             />
             ) : null
-            } */}
-            <h2>HEllo</h2>
+            }
+
+            <SearchBar setSearchTerm={setSearchTerm} />
+            <Grid header='Popular Movies'>
+                {state?.results.map(movie => (
+                    <Thumb
+                        key={movie.id}
+                        clickable
+                        image={movie.poster_path
+                            ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                            : NoImage
+                        }
+                        movieId={movie.id}
+                    />
+                ))}
+            </Grid>
+            <Spinner />
         </>
     )
 }
